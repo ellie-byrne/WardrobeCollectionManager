678b0ff469209a7d5c1abcb4b7974f4a4728d8a2
Ellie Byrne <elliebyrne@carthus.local>
2024-11-29 15:28:37 +0000

    Initial commit

---
+++ src/test/java/dev/wardrobe/WardrobeCollectionManager/WardrobeCollectionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wardrobe.WardrobeCollectionManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeCollectionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
