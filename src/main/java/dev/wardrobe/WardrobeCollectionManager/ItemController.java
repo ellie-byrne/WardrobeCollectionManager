792a59dcf17128c1baab379c9d2aebe08bd667ed
Ellie Byrne <elliebyrne@carthus.local>
2024-12-10 15:26:02 +0000

    web connection fix

--- src/main/java/dev/wardrobe/WardrobeCollectionManager/ItemController.java
+++ src/main/java/dev/wardrobe/WardrobeCollectionManager/ItemController.java
@@ -3,14 +3,12 @@ package dev.wardrobe.WardrobeCollectionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v1/items")
 public class ItemController {
