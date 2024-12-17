be319194c057cad4455dd7218f246481b5f8fab7
Ellie Byrne <elliebyrne@carthus.local>
2024-12-17 13:38:02 +0000

    Basic shell of website

--- React Front End/wardrobe-v1.tmp/src/App.jsx
+++ React Front End/wardrobe-v1.tmp/src/App.jsx
@@ -1,42 +1,21 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios';
 import Layout from './components/Layout';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './components/home/Home';
 
-function App() {
-  const [count, setCount] = useState(0);
 
-//   const wardrobejson = useState([]);
-const [wardrobejson, setWardrobejson] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:8080/api/v1/items').then((response) => {
-        setWardrobejson(response.data);
-        console.log("data", response.data);
-        getItems();
-    })
-    .catch(error => {
-        console.log(error);
-    })
-}, []);
-
-function App() {
-    const [items, setItems] = useState([]);
-
-    const getItems = async () => {
-        const response = await fetch("http://localhost:8080/api/v1/items");
-    }
-
+const App =() => {
     return (
-        wardrobejson.map((e) => (
-            <div>
-                {e.item}
-            </div>
-        ))
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Home/>}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
-}}
+}
 
 export default App;
--- React Front End/wardrobe-v1.tmp/src/api/axiosConfig.js
+++ React Front End/wardrobe-v1.tmp/src/api/axiosConfig.js
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'http://localhost:3000',
-    headers: {"ngrok-skip-browser-warning": "true"}
+    headers: {
+        "ngrok-skip-browser-warning":
+        "true"
+    }
 });
